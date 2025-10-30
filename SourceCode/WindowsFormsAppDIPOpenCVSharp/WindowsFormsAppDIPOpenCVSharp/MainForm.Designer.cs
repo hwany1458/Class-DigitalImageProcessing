f4bb6034e2a5a74e078ccb20aa5ccc5f83deefd9
hwany1458 <hwany1458@empal.com>
2025-10-31 08:38:31 +0900

    upload 2025Csharp

---
+++ SourceCode/WindowsFormsAppDIPOpenCVSharp/WindowsFormsAppDIPOpenCVSharp/MainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsAppDIPOpenCVSharp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.영상처리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.반전ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.PB_InputImage = new System.Windows.Forms.PictureBox();
+            this.PB_OutputImage = new System.Windows.Forms.PictureBox();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.속성정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.대칭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.좌우ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.상하ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.원점ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.히스토그램ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.히스토그램평활화ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.감마보정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_InputImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_OutputImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.영상처리ToolStripMenuItem,
+            this.보기ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem,
+            this.저장ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.열기ToolStripMenuItem.Text = "열기";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장ToolStripMenuItem.Text = "저장";
+            this.저장ToolStripMenuItem.Click += new System.EventHandler(this.저장ToolStripMenuItem_Click);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // 영상처리ToolStripMenuItem
+            // 
+            this.영상처리ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.반전ToolStripMenuItem,
+            this.대칭ToolStripMenuItem,
+            this.히스토그램ToolStripMenuItem,
+            this.히스토그램평활화ToolStripMenuItem,
+            this.감마보정ToolStripMenuItem});
+            this.영상처리ToolStripMenuItem.Name = "영상처리ToolStripMenuItem";
+            this.영상처리ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.영상처리ToolStripMenuItem.Text = "영상처리";
+            // 
+            // 반전ToolStripMenuItem
+            // 
+            this.반전ToolStripMenuItem.Name = "반전ToolStripMenuItem";
+            this.반전ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.반전ToolStripMenuItem.Text = "반전";
+            this.반전ToolStripMenuItem.Click += new System.EventHandler(this.반전ToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.PB_InputImage, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PB_OutputImage, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 36);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 385);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // PB_InputImage
+            // 
+            this.PB_InputImage.Location = new System.Drawing.Point(3, 3);
+            this.PB_InputImage.Name = "PB_InputImage";
+            this.PB_InputImage.Size = new System.Drawing.Size(380, 379);
+            this.PB_InputImage.TabIndex = 0;
+            this.PB_InputImage.TabStop = false;
+            // 
+            // PB_OutputImage
+            // 
+            this.PB_OutputImage.Location = new System.Drawing.Point(391, 3);
+            this.PB_OutputImage.Name = "PB_OutputImage";
+            this.PB_OutputImage.Size = new System.Drawing.Size(382, 379);
+            this.PB_OutputImage.TabIndex = 1;
+            this.PB_OutputImage.TabStop = false;
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.속성정보ToolStripMenuItem});
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.보기ToolStripMenuItem.Text = "보기";
+            // 
+            // 속성정보ToolStripMenuItem
+            // 
+            this.속성정보ToolStripMenuItem.Name = "속성정보ToolStripMenuItem";
+            this.속성정보ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.속성정보ToolStripMenuItem.Text = "속성정보";
+            // 
+            // 대칭ToolStripMenuItem
+            // 
+            this.대칭ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.상하ToolStripMenuItem,
+            this.좌우ToolStripMenuItem,
+            this.원점ToolStripMenuItem});
+            this.대칭ToolStripMenuItem.Name = "대칭ToolStripMenuItem";
+            this.대칭ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.대칭ToolStripMenuItem.Text = "대칭";
+            // 
+            // 좌우ToolStripMenuItem
+            // 
+            this.좌우ToolStripMenuItem.Name = "좌우ToolStripMenuItem";
+            this.좌우ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.좌우ToolStripMenuItem.Text = "좌우";
+            // 
+            // 상하ToolStripMenuItem
+            // 
+            this.상하ToolStripMenuItem.Name = "상하ToolStripMenuItem";
+            this.상하ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.상하ToolStripMenuItem.Text = "상하";
+            this.상하ToolStripMenuItem.Click += new System.EventHandler(this.상하ToolStripMenuItem_Click);
+            // 
+            // 원점ToolStripMenuItem
+            // 
+            this.원점ToolStripMenuItem.Name = "원점ToolStripMenuItem";
+            this.원점ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.원점ToolStripMenuItem.Text = "상하좌우";
+            // 
+            // 히스토그램ToolStripMenuItem
+            // 
+            this.히스토그램ToolStripMenuItem.Name = "히스토그램ToolStripMenuItem";
+            this.히스토그램ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.히스토그램ToolStripMenuItem.Text = "히스토그램";
+            this.히스토그램ToolStripMenuItem.Click += new System.EventHandler(this.히스토그램ToolStripMenuItem_Click);
+            // 
+            // 히스토그램평활화ToolStripMenuItem
+            // 
+            this.히스토그램평활화ToolStripMenuItem.Name = "히스토그램평활화ToolStripMenuItem";
+            this.히스토그램평활화ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.히스토그램평활화ToolStripMenuItem.Text = "히스토그램평활화";
+            this.히스토그램평활화ToolStripMenuItem.Click += new System.EventHandler(this.히스토그램평활화ToolStripMenuItem_Click);
+            // 
+            // 감마보정ToolStripMenuItem
+            // 
+            this.감마보정ToolStripMenuItem.Name = "감마보정ToolStripMenuItem";
+            this.감마보정ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.감마보정ToolStripMenuItem.Text = "감마보정";
+            this.감마보정ToolStripMenuItem.Click += new System.EventHandler(this.감마보정ToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "DIPwithOpenCVSharp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_InputImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_OutputImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox PB_InputImage;
+        private System.Windows.Forms.PictureBox PB_OutputImage;
+        private System.Windows.Forms.ToolStripMenuItem 영상처리ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 반전ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 속성정보ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 대칭ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 좌우ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 상하ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 원점ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 히스토그램ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 히스토그램평활화ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 감마보정ToolStripMenuItem;
+    }
+}
---
+++ SourceCode/WindowsFormsAppDIPOpenCVSharp/WindowsFormsAppDIPOpenCVSharp/MainForm.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace WindowsFormsAppDIPOpenCVSharp
+{
+    public partial class MainForm : Form
+    {
+        // 변수 선언 (전역변수)
+        Mat inputImage = null, outputImage = null;
+        string fileName;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    //Mat inputImage = Cv2.ImRead(dlg.FileName);
+                    // 전역변수로 변경 (다른 메소드에서 함께 사용하려면)
+                    inputImage = Cv2.ImRead(dlg.FileName);
+
+                    PB_InputImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(inputImage);
+                    PB_InputImage.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    // 우측 PictureBox는 클리어
+                    PB_OutputImage.Image = null;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "JPEG Image|*.jpg|PNG Image|*.png";
+            sfd.Title = "Save the filtered image";
+            sfd.ShowDialog();
+
+            if (sfd.FileName != "")
+            {
+                Bitmap filteredImage = (Bitmap)PB_OutputImage.Image;
+                if (sfd.FilterIndex == 1)
+                {
+                    filteredImage.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                else if (sfd.FilterIndex == 2)
+                {
+                    filteredImage.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+                else
+                {
+                    filteredImage.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+            }
+
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void 상하ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage != null)
+            {
+                outputImage = new Mat();
+
+                Cv2.Flip(inputImage, outputImage, FlipMode.X);
+                // FlopMode 옵션 확인하기
+                PB_OutputImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(outputImage);
+                PB_OutputImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Input Image is NOT ready ...");
+            }
+        }
+
+        private void 히스토그램ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage != null)
+            {
+                // 다른 메소드에서 활용하기 위해 함수로 변경
+                GetImageHistogram(inputImage);
+            }
+            else
+            {
+                MessageBox.Show("Input Image is NOT ready ...");
+            }
+        }
+
+        private void 반전ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage != null)
+            {
+                outputImage = new Mat();
+
+                Cv2.BitwiseNot(inputImage, outputImage);
+                // cv.Bitwise [And | Or | Xor | Not] 확인하기
+                PB_OutputImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(outputImage);
+                PB_OutputImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Input Image is NOT ready ...");
+            }
+        }
+
+        private void 히스토그램평활화ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage != null)
+            {
+                Mat grey = new Mat();    // 흑백이미지
+                if (inputImage.Channels() == 3) { 
+                    grey = inputImage.CvtColor(ColorConversionCodes.BGR2GRAY); 
+                }
+                else { 
+                    grey = inputImage; 
+                }
+
+                // equalize the histogram
+                Mat histoEqualizedImage = new Mat();
+                Cv2.EqualizeHist(grey, histoEqualizedImage);
+
+                // 히스토그램 평활화 결과를 (우측) picture box 에 뿌려줌
+                PB_OutputImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(histoEqualizedImage);
+                PB_OutputImage.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                // Histogram view
+                //GetHistogram(histoEqualizedImage);
+            }
+            else
+            {
+                MessageBox.Show("Input Image is NOT ready ...");
+            }
+        }
+
+        private void 감마보정ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage != null)
+            {
+                outputImage = new Mat();
+
+                double gamma_value = 0.5;
+                // 감마값이 0.5, 1.0, 2.0, 2.2, 3.0, 4.0일때 결과를 확인하기
+                byte[] lut = new byte[256];
+
+                for (int i = 0; i < lut.Length; i++)
+                {
+                    lut[i] = (byte)(Math.Pow(i / 255.0, 1.0 / gamma_value) * 255.0);
+                }
+
+                //Cv.LUT(src, gamma, lut);
+                Cv2.LUT(inputImage, lut, outputImage);
+
+
+                PB_OutputImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(outputImage);
+                PB_OutputImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Input Image is NOT ready ...");
+            }
+        }
+
+        // ------
+        private void GetImageHistogram(Mat inputImage)
+        {
+            Mat grey = new Mat();  // 흑백이미지
+            if (inputImage.Channels() == 3)
+            {
+                grey = inputImage.CvtColor(ColorConversionCodes.BGR2GRAY);
+            }
+            else
+            {
+                grey = inputImage;
+            }
+
+
+            // Histogram view
+            const int width = 260, height = 200;
+            Mat render = new Mat(new OpenCvSharp.Size(width, height), MatType.CV_8UC3, Scalar.All(255));
+
+            // calculate histogram
+            Mat hist = new Mat();   //히스토그램
+            int[] hdims = { 256 };
+            Rangef[] ranges = { new Rangef(0, 256), };  // min max
+            Cv2.CalcHist(new Mat[] { grey }, new int[] { 0 }, null, hist, 1, hdims, ranges);
+
+            // Get the max value of histogram
+            double minVal, maxVal;
+            Cv2.MinMaxLoc(hist, out minVal, out maxVal);
+
+            Scalar color = Scalar.All(100);
+
+            // scales and draws histogram
+            hist = hist * (maxVal != 0 ? height / maxVal : 0.0);
+            for (int i = 0; i < hdims[0]; i++)
+            {
+                int binW = (int)((double)width / hdims[0]);
+                render.Rectangle(
+                    new OpenCvSharp.Point(i * binW, render.Rows - (int)hist.Get<float>(i)),
+                    new OpenCvSharp.Point((i + 1) * binW, render.Rows),
+                    color,
+                    -1);
+            }
+            new Window("Image", grey);
+            new Window("Histogram", render);
+
+            Cv2.WaitKey();
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
